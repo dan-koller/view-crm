058f037c5a0c61ec97e3b2f81d3c4b75fe40bbba
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2023-08-22 09:53:09 +0200

    Updated the account controller to handle the user name and fixed a bug that crashed the app when assigning roles to new users

--- View.WebApi/Controllers/AccountController.cs
+++ View.WebApi/Controllers/AccountController.cs
@@ -46,6 +46,7 @@ public class AccountController : ControllerBase
                 Message = "Invalid Email or Password."
             });
         var secToken = await _jwtHandler.GetTokenAsync(user);
+        secToken.Payload["Name"] = user.Name;
         var jwt = new JwtSecurityTokenHandler().WriteToken(secToken);
         return Ok(new LoginResult()
         {
@@ -59,9 +60,9 @@ public class AccountController : ControllerBase
     [HttpPost("register")]
     [ProducesResponseType(200, Type = typeof(string))]
     [ProducesResponseType(400)]
-    public async Task<IActionResult> Register(LoginRequest loginRequest)
+    public async Task<IActionResult> Register(RegisterRequest registerRequest)
     {
-        var user = await _userManager.FindByNameAsync(loginRequest.Email);
+        var user = await _userManager.FindByNameAsync(registerRequest.Email);
         if (user != null)
             return Unauthorized(new LoginResult()
             {
@@ -71,23 +72,39 @@ public class AccountController : ControllerBase
         var newUser = new ApplicationUser()
         {
             SecurityStamp = Guid.NewGuid().ToString(),
-            UserName = loginRequest.Email,
-            Email = loginRequest.Email,
+            UserName = registerRequest.Email,
+            Email = registerRequest.Email,
+            Name = registerRequest.Name
         };
 
+        // setup the default role names
+        string role_RegisteredUser = "RegisteredUser";
+        string role_Administrator = "Administrator";
+
+        // create the default roles (if they don't exist yet)
+        if (await _roleManager.FindByNameAsync(role_RegisteredUser) == null)
+        {
+            await _roleManager.CreateAsync(new IdentityRole(role_RegisteredUser));
+        }
+        if (await _roleManager.FindByNameAsync(role_Administrator) == null)
+        {
+            await _roleManager.CreateAsync(new IdentityRole(role_Administrator));
+        }
+
         // create the new user
-        var isCreated = await _userManager.CreateAsync(newUser, loginRequest.Password);
+        var isCreated = await _userManager.CreateAsync(newUser, registerRequest.Password);
 
         if (isCreated.Succeeded)
         {
             // the first registered user will be an admin in addition to a registered user
-            if (_userManager.Users.Count() == 0)
+            if (_userManager.Users.Count() == 1)
             {
-                await _userManager.AddToRolesAsync(newUser, new List<string>() { "Administrator", "RegisteredUser" });
+                await _userManager.AddToRoleAsync(newUser, role_Administrator);
+                await _userManager.AddToRoleAsync(newUser, role_RegisteredUser);
             }
             else
             {
-                await _userManager.AddToRoleAsync(newUser, "RegisteredUser");
+                await _userManager.AddToRoleAsync(newUser, role_RegisteredUser);
             }
 
             // confirm the e-mail and remove lockout
@@ -98,6 +115,8 @@ public class AccountController : ControllerBase
             await _userManager.UpdateAsync(newUser);
 
             var secToken = await _jwtHandler.GetTokenAsync(newUser);
+            // Include the name as a claim in the JWT token
+            secToken.Payload["Name"] = newUser.Name;
             var jwt = new JwtSecurityTokenHandler().WriteToken(secToken);
             return Ok(new LoginResult()
             {
