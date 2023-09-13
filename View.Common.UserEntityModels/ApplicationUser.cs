cf42c1adf3aaa3d3e51f3c629aed85d14ea8a882
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2023-09-13 10:21:51 +0200

    The UserEntityModel is now being used for both sqlite and sqlserver

--- View.Common.UserEntityModels/ApplicationUser.cs
+++ View.Common.UserEntityModels/ApplicationUser.cs
@@ -2,6 +2,7 @@
 
 namespace View.Shared;
 
+// The same model is used for both the Sqlite and SqlServer databases
 public class ApplicationUser : IdentityUser
 {
     public string? Name { get; set; }
