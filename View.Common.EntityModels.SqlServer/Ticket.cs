205638c152bbf48cd72b817027081dd803f492ea
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2023-09-14 13:56:14 +0200

    Removed debug comments after migrating tickets to sqlserver

--- View.Common.EntityModels.SqlServer/Ticket.cs
+++ View.Common.EntityModels.SqlServer/Ticket.cs
@@ -11,48 +11,48 @@ public partial class Ticket
 {
     [Key]
     [Column("id")]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Specify identity column for SQL Server
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
 
     [Column("category")]
-    [MaxLength(255)] // Set an appropriate max length for SQL Server
+    [MaxLength(255)]
     public string? Category { get; set; }
 
     [Column("color")]
-    [MaxLength(255)] // Set an appropriate max length for SQL Server
+    [MaxLength(255)]
     public string? Color { get; set; }
 
     [Column("title")]
-    [MaxLength(255)] // Set an appropriate max length for SQL Server
+    [MaxLength(255)]
     public string? Title { get; set; }
 
     [Column("owner")]
-    [MaxLength(255)] // Set an appropriate max length for SQL Server
+    [MaxLength(255)]
     public string? Owner { get; set; }
 
     [Column("avatar")]
-    [MaxLength(255)] // Set an appropriate max length for SQL Server
+    [MaxLength(255)]
     public string? Avatar { get; set; }
 
     [Column("status")]
-    [MaxLength(255)] // Set an appropriate max length for SQL Server
+    [MaxLength(255)]
     public string? Status { get; set; }
 
     [Column("priority")]
-    public int? Priority { get; set; } // Use int for priority, adjust data type as needed
+    public int? Priority { get; set; }
 
     [Column("progress")]
-    public int? Progress { get; set; } // Use int for progress, adjust data type as needed
+    public int? Progress { get; set; }
 
     [Column("description")]
-    [MaxLength(1000)] // Set an appropriate max length for SQL Server
+    [MaxLength(1000)]
     public string? Description { get; set; }
 
     [Column("timestamp")]
-    public DateTime? Timestamp { get; set; } // Use DateTime for timestamp
+    public DateTime? Timestamp { get; set; }
 
     [Column("notes")]
-    [MaxLength(2000)] // Set an appropriate max length for SQL Server
+    [MaxLength(2000)]
     public string? Notes { get; set; }
 
     [Column("closed")]
