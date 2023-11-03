f697243e29c7381a1ef83891946e9afa080bf2ad
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2023-11-03 08:19:49 +0100

    Added test ordering based on priority
    This will make testing of individual components and methods more easy for this project

---
+++ src/View.UnitTests/TestPriorityAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace View.UnitTests;
+
+// Used to create a priority attribute for xUnit tests
+// so that they can be ordered
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class TestPriorityAttribute : Attribute
+{
+    public TestPriorityAttribute(int priority)
+    {
+        Priority = priority;
+    }
+
+    public int Priority { get; private set; }
+}
+
